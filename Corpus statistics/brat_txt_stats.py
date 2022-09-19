7d4e42574c0ba57199245a3e8ccd52cabd0405b4
katnastou <katnastou@biol.uoa.gr>
2022-09-19 20:02:04 +0200

    Update to read from current directory structure

--- Corpus statistics/brat_txt_stats.py
+++ Corpus statistics/brat_txt_stats.py
@@ -28,7 +28,8 @@ def main(argv):
     '''example call: python3 brat_txt_stats.py --dir brat-annotation-standoff-txts'''
     args = argparser().parse_args()
     docs, words, chars = 0, 0, 0
-    for filename in glob.glob(os.path.join(args.dir, '*.txt')):
+    #directory structure entire-corpus/{test,dev,train} --> '*'
+    for filename in glob.glob(os.path.join(args.dir, '*', '*.txt')):
         with open(os.path.join(os.getcwd(), filename), 'r') as fn:
             text = fn.read()
             file_docs, file_words, file_chars = get_txt_stats(text)
